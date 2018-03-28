1f11247bc979aae3b101abb7c66b9d7f79a15620
Williamhiller <1747768963@qq.com>
2018-03-28 16:57:05 +0800

    [进球指数]

--- js/app.js
+++ js/app.js
@@ -122,6 +122,14 @@ app.controller('myCtrl', ['$scope',function(s) {
     s.dataVisitIndex = [];
     s.dataHistoryIndex = [];
 
+    // 进球指数
+    s.goalIndex = {
+        average : 0,
+        left : 0,
+        right : 0
+    };
+    s.dataGoalIndex = [];
+
     for(var i=0;i<6;i++){
         s.dataHomeIndex.push({
             l : '',
@@ -135,10 +143,15 @@ app.controller('myCtrl', ['$scope',function(s) {
             l : '',
             r : ''
         });
+        s.dataGoalIndex.push({
+            l : '',
+            r : ''
+        });
     }
 
     s.$watch('dataHomeIndex',function (newVal, oldVal) {
         var homeIndex = 0;
+        var weigh = 0;
         newVal.map(function (item,index) {
             var val = (item.l !== ''? item.l : item.r !== ''?item.r : 0);
             homeIndex += val * (6-index)
@@ -153,6 +166,7 @@ app.controller('myCtrl', ['$scope',function(s) {
         });
         s.visitIndex = (visitIndex*100/63).toFixed(0);
     },true);
+
     s.$watch('dataHistoryIndex',function (newVal, oldVal) {
         var hitoryIndex = 0;
         newVal.map(function (item,index) {
@@ -160,6 +174,43 @@ app.controller('myCtrl', ['$scope',function(s) {
             hitoryIndex += val * (6-index)
         });
         s.historyIndex = (hitoryIndex*100/21).toFixed(0);
+    },true);
+
+    s.$watch('dataGoalIndex',function (newVal, oldVal) {
+        var left = [];
+        var right = [];
+        var ave_l =0 , imp_l =0;
+        var ave_r =0 , imp_r =0;
+        newVal.map(function (item,index) {
+            if(item.l !== '') {
+                left.push(item.l)
+            }
+            if(item.r !== '') {
+                right.push(item.r)
+            }
+        });
+        left.map(function (item,index) {
+            ave_l += item*(left.length - index);
+            imp_l += index+1;
+        });
+        right.map(function (item,index) {
+            ave_r += item*(right.length - index);
+            imp_r += index+1;
+        });
+
+        s.goalIndex.left = ave_l/imp_l;
+        s.goalIndex.right = ave_r/imp_r;
+
+
+        if(s.goalIndex.left.toString() === 'NaN') {
+            s.goalIndex.left = 0;
+        }
+        if(s.goalIndex.right.toString() === 'NaN') {
+            s.goalIndex.right = 0;
+        }
+
+        s.goalIndex.average = (s.goalIndex.left + s.goalIndex.right)/2;
+
     },true);
 
 }]);
