af71ff831d61e89e068b01ed5cfd27f04b524482
Williamhiller <1747768963@qq.com>
2018-07-24 08:09:06 +0800

    高级模式

--- spider/test.js
+++ spider/test.js
@@ -3,10 +3,19 @@
  */
 var cheerio = require('cheerio');
 var superagent = require('superagent');
+var charset = require("superagent-charset");
+charset(superagent); //设置字符
 
-// var url = "http://odds.500.com/fenxi/shuju-733800.shtml";
-var url = "http://m.win007.com/analy/Analysis/1510761.htm";
-superagent.get('https://cnodejs.org/')
+// 球探获取联赛数据
+//http://zq.win007.com/jsData/matchResult/2017-2018/s36.js?version=2018072404
+
+
+// var url = "http://vip.win007.com/changeDetail/handicap.aspx?id=1509904&companyID=8&l=0";
+// var url = "http://zq.win007.com/big/League/2017-2018/36.html";
+var url = "http://zq.win007.com/jsData/matchResult/2017-2018/s36.js?version=2018072404";
+superagent.get(url)
+    .charset('utf-8')
+    .buffer(true)
     .end(function (err, sres) {
         if (err) {
             console.log(err);
@@ -14,7 +23,7 @@ superagent.get('https://cnodejs.org/')
         }
         var $ = cheerio.load(sres.text, {decodeEntities: false});
 
-        console.log($("body").html())
+        console.log(sres)
 
     });
 
